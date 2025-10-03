eba197aa06b7f5fa2579779d111341d5202ccd7d
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2025-10-03 12:44:49 +0300

    signup and login get url from .env

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 export default function useLogin(setIsAuthenticated) {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+  const website = process.env.API_URL;
   const url = "/api/users/login";
   const navigate = useNavigate();
 
   const login = async (object) => {
     setIsLoading(true);
     setError(null);
-    const response = await fetch(url, {
+    const response = await fetch(website + url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(object),
--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 export default function useSignup(setIsAuthenticated) {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+  const website = process.env.API_URL;
   const url = "/api/users/signup";
   const navigate = useNavigate();
 
   const signup = async (object) => {
     setIsLoading(true);
     setError(null);
-    const response = await fetch(url, {
+    const response = await fetch(website + url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(object),
